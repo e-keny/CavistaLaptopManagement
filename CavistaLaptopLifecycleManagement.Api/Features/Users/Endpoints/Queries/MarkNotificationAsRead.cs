de85315b83bf9201196dfd4cc47910b6e3b7c3e3
Ekene <eobiasogu@axxess.com>
2026-09-09 08:53:08 +0100

    Rename EndPoint

--- CavistaLaptopLifecycleManagement.Api/Features/Users/Endpoints/Queries/MarkNotificationAsRead.cs
+++ CavistaLaptopLifecycleManagement.Api/Features/Users/Endpoints/Queries/MarkNotificationAsRead.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 namespace CavistaLaptopLifecycleManagement.Api.Features.Users.Endpoints.Queries
 {
     [Handler]
-    [MapGet("notification/{notificationId}")]
+    [MapGet("mark-as-read/{notificationId}")]
     [MapGroup<UserMapGroup>]
     public static partial class MarkNotificationAsRead
     {
